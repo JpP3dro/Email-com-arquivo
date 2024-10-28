4cc4e5786b5da63f229560ddf0978744b0ac2d6c
JpP3dro <joaopedrodallessiodebarros@gmail.com>
2024-10-27 21:59:49 -0300

    Vendo se o diretório arquivos existe, se não, ele é criado

--- enviar.php
+++ enviar.php
@@ -14,6 +14,7 @@ if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
+
 require './biblioteca/vendor/autoload.php';
 
 //Criando uma variável do tipo PHPMailer
@@ -21,59 +22,63 @@ $mail = new PHPMailer(true);
 try {
     //Configurações do SMTP
     //$mail->SMTPDebug = SMTP::DEBUG_SERVER; (deixe essa opção caso queira ver os detalhes do envio)
-    $mail->SMTPDebug = 0;                     
-    $mail->isSMTP();                                            
-    $mail->Host       = 'smtp.gmail.com';                     
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com';
     $mail->SMTPAuth   = true;
-    
+
     //Conta e senha do email que vai enviar a mensagem
-    $mail->Username   = 'example@gmail.com';                     
-    $mail->Password   = 'app password';                               
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;            
+    $mail->Username   = 'example@gmail.com';
+    $mail->Password   = 'app password';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
     $mail->Port       = 587;
-    $mail->Timeout = 60;          
-    
+    $mail->Timeout = 60;
+
     //Remetente e destinatário
     $mail->setFrom('example@gmail.com', 'Remetente');
-    $mail->addAddress($email, 'Destinatário');                  
+    $mail->addAddress($email, 'Destinatário');
 
     //Definindo o HTML do email como true
-    $mail->isHTML(true);                 
-    
+    $mail->isHTML(true);
+
     //Assunto, mensagem e a mensagem alternativa (caso o HTML não esteja funcionando)
     $mail->Subject = $assunto;
     $mail->Body    = $mensagem;
     $mail->AltBody = strip_tags($mensagem);
 
     //Se o usuário tiver colocado um arquivo
-    if(isset($_FILES['arquivo']) && $_FILES['arquivo']['size'] > 0){
+    if (isset($_FILES['arquivo']) && $_FILES['arquivo']['size'] > 0) {
 
         //Se não houve nenhum erro ao fazer o upload do arquivo
         if ($_FILES['arquivo']['error'] === UPLOAD_ERR_OK) {
 
-        //Pegando alguns atributos do arquivo e definindo o diretório
-        $arquivo = $_FILES['arquivo'];
-        $nome_arquivo = $arquivo['name'];
-        $tmp_nome = $arquivo['tmp_name']; 
-        $diretorio = "arquivos/"; 
+            //Se o diretório não existir, ele cria o diretório
+            $diretorio = __DIR__ . "/arquivos/"; 
+            if (!file_exists($diretorio)) {
+                mkdir($diretorio, 0755, true);
+            }
 
-        //Move o arquivo para a pasta criada para salvar os arquivos
-        move_uploaded_file($tmp_nome, $diretorio . $nome_arquivo); 
+            //Pegando alguns atributos do arquivo e definindo o diretório
+            $arquivo = $_FILES['arquivo'];
+            $nome_arquivo = $arquivo['name'];
+            $tmp_nome = $arquivo['tmp_name'];
+            $diretorio = "arquivos/";
 
-        //Adiciona o arquivo ao email
-        $mail->addAttachment($diretorio . $nome_arquivo);  
+            //Move o arquivo para a pasta criada para salvar os arquivos
+            move_uploaded_file($tmp_nome, $diretorio . $nome_arquivo);
+
+            //Adiciona o arquivo ao email
+            $mail->addAttachment($diretorio . $nome_arquivo);
         } else {
             echo "Erro ao fazer o upload do arquivo";
         }
     }
-    
+
     //Envia o email, exibe uma mensagem de sucesso e exclui o arquivo do diretório
     $mail->send();
     echo "E-mail enviado com sucesso!";
     unlink($diretorio . $nome_arquivo);
-} 
-catch (Exception $e) {
-    echo("Erro ao enviar o e-mail: {$mail->ErrorInfo}");
+} catch (Exception $e) {
+    echo ("Erro ao enviar o e-mail: {$mail->ErrorInfo}");
 }
-
 ?>
